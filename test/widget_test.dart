99be2da8ca23d36c03a1734bc807763cf96c3e20
sacha <sacha.arbonel@hotmail.fr>
2019-10-23 17:53:48 +0200

    first commit

---
+++ test/widget_test.dart
@@ -0,0 +1,39 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:mocking_http_example/main.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+import 'package:test/test.dart';
+
+class MockClient extends Mock implements http.Client {}
+
+void main() {
+  group('fetchPost', () {
+    test('returns a Post if the http call completes successfully', () async {
+      final client = MockClient();
+
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
+      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
+          .thenAnswer((_) async => http.Response('{"title": "Test"}', 200));
+
+      expect(await fetchPost(client), const TypeMatcher<Post>());
+    });
+
+    test('throws an exception if the http call completes with an error', () {
+      final client = MockClient();
+
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
+      when(client.get('https://jsonplaceholder.typicode.com/posts/1'))
+          .thenAnswer((_) async => http.Response('Not Found', 404));
+
+      expect(fetchPost(client), throwsException);
+    });
+  });
+}
